a350be0d8ac379e53f3116168d1396c4c05d5f03
Aaditya Revandkar <aadityarevandkar@gmail.com>
2025-10-21 10:47:47 +0530

    Made working on vps hosting

---
+++ config/system-addons.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'siptsgcr',
+    'purchase_key' => '7a7b2216-4acb-4dc6-b243-99edabf3ba66',
+    'software_id' => 'MzY3NzIxMTI=',
+    'domain' => 'localhost/Backend-6amMart/public',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => 1,
+    'username' => 'sixamtech',
+    'purchase_key' => 'e1ccbbcb-73ad-44fa-971d-aa2af14033b0',
+    'software_id' => 'MzY3NzIxNzM=',
+    'domain' => '6ammart.test',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => 1,
+    'username' => 'sixamtech',
+    'purchase_key' => '49413560-a3f3-4ee6-bacb-3ca8041c3629',
+    'software_id' => 'MzY3NzIxNDg=',
+    'domain' => '6ammart.test',
+    'software_type' => 'addon',
+  ),
+  'react_web' => 
+  array (
+    'active' => '1',
+    'username' => 'siptsgcr',
+    'purchase_key' => '7a7b2216-4acb-4dc6-b243-99edabf3ba66',
+    'software_id' => 'NDUzNzAzNTE=',
+    'domain' => 'localhost/Backend-6amMart/public',
+    'software_type' => 'addon',
+  ),
+);
